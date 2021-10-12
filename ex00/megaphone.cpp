8280241496f8b2134359cbcb154c819ace2fa0e1
ferundal <ferundal@gmail.com>
2021-10-12 11:34:27 +0300

    all shoud works

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int	is_lower_case(char c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
+
+int	main(int argc, char** argv)
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	}
+	else
+	{
+		int pos;
+		while (argc > 1)
+		{
+			pos = 0;
+			++argv;
+			while (*argv[pos] != '\0')
+			{
+				if (is_lower_case(*argv[pos]) == true)
+					std::cout << *argv[pos] - 32;
+				else
+					std::cout << *argv[pos];
+			}
+			std::cout << std::endl;
+			--argc;
+		}
+	}
+}
